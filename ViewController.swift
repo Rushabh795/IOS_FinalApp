a70e6b8c689f1d6e18fcbe8796a7b0a2b4e36d09
rushabhshah <rushabh795@gmail.com>
2022-12-14 18:38:57 -0500

    #2 Added Project into the repository

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodApp
+//
+//  Created by Rushabh Shah on 10/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+   
+
+}
+
