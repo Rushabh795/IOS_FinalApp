73851de2bc4935b4c6826b4720b05479533384d2
rushabhshah <rushabh795@gmail.com>
2022-12-14 18:45:54 -0500

    #3 Create API call
    - Create an API call for fetching data from the API and Print it.

--- ViewController.swift
+++ ViewController.swift
@@ -11,9 +11,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchFruitsListFromApiService()
     }
 
-   
+    func fetchFruitsListFromApiService() {
+            let url = URL(string: "https://www.fruityvice.com/api/fruit/all")!
+            
+            URLSession.shared.dataTask(with: url) { data, response, error in
+               
+                
+                do {
+                    let getResponse = try JSONSerialization.jsonObject(with: data!, options: .allowFragments)
+                    
+                    print(getResponse)
+                    
+                } catch {
+                    print("error serializing JSON: \(error)")
+                }
+            }.resume()
+        }
+
 
 }
 
